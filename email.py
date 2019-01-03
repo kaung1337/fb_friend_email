02b21688e8fd1ae4ffdb3ea7d618499d066588e9
khzesdp <46347272+khzesdp@users.noreply.github.com>
2019-01-03 19:07:52 +0630

    Create email.py

---
+++ email.py
@@ -0,0 +1,26 @@
+#-*- coding: utf-8 -*-
+import json
+from facepy import GraphAPI
+
+# kaung htet zaw
+
+
+api = "" # enter your token
+
+
+graph = GraphAPI(api)
+r = graph.get('me/friends?fields=email')
+
+for hee in r['data']:
+        if 'email' not in hee:
+            continue
+        msil =str(hee['id'])+" "+"==="+str(hee['email'])
+        print(mail)
+  
+ 
+
+     
+ 
+   
+ 
+    
